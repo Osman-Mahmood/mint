6442f1d14f894a389a74e575c57818b25adbd953
Osman-Mahmood <usmanmahmood7323@gmail.com>
2023-06-10 01:01:41 +0500

    first commit

--- src/components/Eth/Eth.jsx
+++ src/components/Eth/Eth.jsx
@@ -4,14 +4,31 @@ const Eth = () => {
   return (
     <div className='container pt-lg-5 pt-2 mt-lg-5 mt-0 pb-5 mtb-5'>
         <div className='row'>
-            <div className='col-lg-12'>
-                <ul className='text-white p-5'>
-                    <li> An open-source and non-custodial public good >> born with the purpose and determination of enhancing the digital asset industry.</li>
-                    <li> A self-organizing distributed system >> using 40% of the smart contract's revenue to fund Public Goods.</li>
-                    <li>A self-sustained ecosystem >> using 30% of the smart contract's revenue to reward its end-users.</li>
+            <div className='col-lg-4'>
+                {/* <ul className='box p-5 text-dark fs-5'>
+                    <li> <strong>An open-source and non-custodial public good >></strong> born with the purpose and determination of enhancing the digital asset industry.</li>
+                    <li><strong>A self-organizing distributed system >></strong>  using 40% of the smart contract's revenue to fund Public Goods.</li>
+                    <li><strong>A self-sustained ecosystem >> </strong>using 30% of the smart contract's revenue to reward its end-users.</li>
                   
-                </ul>
-            </div>
+                </ul> */}
+                <div className='box_2'>
+                    <h5>An open-source and non-custodial public good</h5>
+                <p className='text-dark'> born with the purpose and determination of enhancing the digital asset industry.</p>
+                </div>
+                </div>
+                <div className='col-lg-4'>
+                <div className='box_2'>
+                    <h5>A self-organizing distributed system</h5>
+                <p className='text-dark'> using 40% of the smart contract's revenue to fund Public Goods</p>
+                </div>
+                </div>
+                <div className='col-lg-4'>
+                <div className='box_2'>
+                    <h5>A self-sustained ecosystem</h5>
+                <p className='text-dark'> using 30% of the smart contract's revenue to reward its end-users.</p>
+                </div>
+                </div>
+
         </div>
       
     </div>
