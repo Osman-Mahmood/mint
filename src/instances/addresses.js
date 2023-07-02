2c177c7889068f80e2ae962f993a911962dc7596
JawwadAhmadDev <jawwadahmad.edu@gmail.com>
2023-07-02 18:28:30 +0500

    address updated

--- src/instances/addresses.js
+++ src/instances/addresses.js
@@ -1,2 +1,2 @@
-export const factoryAddress = "0xd7Cb7Df4532AA4344DBd7CC35AFC69e09101336d"
-export const factoryEthAddresss = "0x33c7737a4c0369019372bF6B2D02EEE66C014661"
+export const factoryAddress = "0xc6afc7a07dd61972b65e04b6f57b2925bf2129bf"
+export const factoryEthAddresss = "0x6b55e13481cb928f5f144d9df68e2f299b3748f2"
