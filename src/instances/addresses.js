7fb4ba4f3659ebb7ce466d97bb90c7672655b9a3
JawwadAhmadDev <jawwadahmad.edu@gmail.com>
2023-07-18 12:41:01 +0500

    georli factory address and abi updated

--- src/instances/addresses.js
+++ src/instances/addresses.js
@@ -1,2 +1,2 @@
-export const factoryAddress = "0xc6afc7a07dd61972b65e04b6f57b2925bf2129bf"
-export const factoryEthAddresss = "0x6b55e13481cb928f5f144d9df68e2f299b3748f2"
+export const factoryAddress = "0xc6afc7a07dd61972b65e04b6f57b2925bf2129bf";
+export const factoryEthAddresss = "0x61c726dc8d65a4592445d26b0aee04a2f4940533";
